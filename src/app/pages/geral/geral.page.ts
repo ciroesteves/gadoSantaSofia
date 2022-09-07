b9909d52d3b79c60d94c1c18ef5532df626e399c
ciroesteves <ciro.esteves@hotmail.com>
2022-09-07 13:55:05 -0300

    inicial searchbar e ordenando lista

--- src/app/pages/geral/geral.page.ts
+++ src/app/pages/geral/geral.page.ts
@@ -2,6 +2,7 @@ import { OperacoesService } from './../../service/operacoes.service';
 import { Gado } from 'src/app/interfaces/gado';
 import { Component, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-geral',
@@ -10,15 +11,43 @@ import { Subscription } from 'rxjs';
 })
 export class GeralPage implements OnInit {
   public animais = new Array<Gado>();
+  gados: any;
   private animaisSubscription: Subscription;
+  public dataAtual;
+  public count = 0;
+  barraPesquisa: string;
 
   constructor(
     private operacoesService: OperacoesService,
   ) {
+    this.barraPesquisa = '';
+    this.dataAtual = new Date();
     this.animaisSubscription = this.operacoesService.getAnimais().subscribe(data => {
+      function compare( a, b ) {
+        if ( a.numero < b.numero ){
+          return -1;
+        }
+        if ( a.numero > b.numero ){
+          return 1;
+        }
+        return 0;
+      }
+      
+      data.sort( compare );
       this.animais = data;
     })
-    
+  }
+
+  filtroAnimal(pesquisa: any){
+    let val = pesquisa.target.value;
+    if(val && val.trim() != ''){
+      this.animais = _.values(this.animais);
+      this.animais.filter((animal) => {
+        return (animal.nome.indexOf(val) > -1);
+      })
+    } else {
+      this.gados = this.animais;
+    }
   }
 
   ngOnInit() {
